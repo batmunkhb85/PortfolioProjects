a61df96832254138585bbad04485af3b802584ac
batmunkhb85 <batmunkhb85@gmail.com>
2022-08-24 18:44:54 +0800

    Add files via upload

---
+++ SQL-Project.sql
@@ -0,0 +1,70 @@
+select * from [Portfolio-Project].dbo.[CovidDeaths]
+order by 3,4
+
+-- select * from [Portfolio-Project].dbo.[CovidVaccinations]
+-- order by 3,4
+
+Select Location, Date, total_cases, new_cases, total_deaths, population
+from [Portfolio-Project].dbo.[CovidDeaths]
+order by 1,2
+
+-- Looking at Total Cases VS Total Deaths
+-- Shows likelyhood of dying if you contract in your country
+
+Select Location, Date, total_cases, total_deaths, (total_deaths/total_cases)*100 as DeathPercentage
+from [Portfolio-Project].dbo.[CovidDeaths]
+where location like '%Mongol%'
+order by 1,2
+
+-- Looking at Total Cases VS Population
+
+Select Location, Date, total_cases, population, (total_cases/population)*100 as CasePercentage
+from [Portfolio-Project].dbo.[CovidDeaths]
+where location like '%mongol%'
+order by 1,2
+
+-- Looking at Countries with Highest Infection Rate Compared to Population
+
+Select Location, population, max(total_cases) as HighestInfectionCount, max(total_cases/population)*100 as PercentPopulationInfected
+from [Portfolio-Project].dbo.[CovidDeaths]
+where continent is not null
+--- where location like '%mongol%'
+group by location, population
+order by PercentPopulationInfected DESC
+
+-- This is Shows Countries with Highest Death Count of Population
+
+Select Location, max(cast(total_deaths as int)) as TotalDeathCount
+from [Portfolio-Project].dbo.[CovidDeaths]
+where continent is not null
+--- where location like '%mongol%'
+group by location
+order by TotalDeathCount DESC
+
+-- LET'S BREAK THINGS DOWN BY CONTINENT
+
+Select location, max(cast(total_deaths as int)) as TotalDeathCount
+from [Portfolio-Project].dbo.[CovidDeaths]
+where location not like '%income%'
+and continent is null
+group by location
+order by TotalDeathCount DESC
+
+-- GLOBAL NUMBERS
+
+Select date, sum(new_cases) as total_cases, sum(cast(new_deaths as int)) as total_deaths, sum(cast(new_deaths as int))/sum(new_cases)*100 as DeathPercentage
+from [Portfolio-Project].dbo.[CovidDeaths]
+where continent is not null
+group by date
+order by 1,2
+
+-- Looking at Total Population VS Vaccineation
+
+select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations 
+--, sum(cast(vac.new_vaccinations as int)) over (partition by dea.location)
+from [Portfolio-Project].dbo.[CovidDeaths] dea
+join [Portfolio-Project].dbo.[CovidVaccinations] vac
+on dea.location = vac.location
+and dea.date = vac.date
+where dea.continent is not null
+order by 2,3
