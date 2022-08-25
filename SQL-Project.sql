10e4aa1e20810c6f32577e2bb7c6c1c4434afe9c
batmunkhb85 <batmunkhb85@gmail.com>
2022-08-25 16:31:37 +0800

    Add files via upload

--- SQL-Project.sql
+++ SQL-Project.sql
@@ -52,19 +52,73 @@ order by TotalDeathCount DESC
 
 -- GLOBAL NUMBERS
 
-Select date, sum(new_cases) as total_cases, sum(cast(new_deaths as int)) as total_deaths, sum(cast(new_deaths as int))/sum(new_cases)*100 as DeathPercentage
+Select sum(new_cases) as total_cases, sum(cast(new_deaths as int)) as total_deaths, sum(cast(new_deaths as int))/sum(new_cases)*100 as DeathPercentage
 from [Portfolio-Project].dbo.[CovidDeaths]
 where continent is not null
-group by date
+--group by date
 order by 1,2
 
 -- Looking at Total Population VS Vaccineation
 
 select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations 
---, sum(cast(vac.new_vaccinations as int)) over (partition by dea.location)
+, sum(cast(vac.new_vaccinations as bigint)) over (partition by dea.location order by dea.location, dea.date) as RollingPeopleVaccinated
 from [Portfolio-Project].dbo.[CovidDeaths] dea
 join [Portfolio-Project].dbo.[CovidVaccinations] vac
 on dea.location = vac.location
 and dea.date = vac.date
 where dea.continent is not null
 order by 2,3
+
+-- Use CTE
+
+with PopVsVac (Continent, Location, Date, Population, New_Vaccinations, RollingPeopleVaccinated)
+as
+(
+select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations 
+, sum(cast(vac.new_vaccinations as bigint)) over (partition by dea.location order by dea.location, dea.date) as RollingPeopleVaccinated
+from [Portfolio-Project].dbo.[CovidDeaths] dea
+join [Portfolio-Project].dbo.[CovidVaccinations] vac
+on dea.location = vac.location
+and dea.date = vac.date
+where dea.continent is not null
+--order by 2,3
+)
+Select *, (RollingPeopleVaccinated/Population)*100
+from PopVsVac
+
+-- Temp Table
+
+Drop table if exists #PercentPopulationVaccinated
+create table #PercentPopulationVaccinated
+(
+Continent nvarchar(255),
+location nvarchar(255),
+date datetime,
+population numeric,
+New_vaccinations numeric,
+RollingPeopleVaccinated numeric
+)
+
+insert into #PercentPopulationVaccinated
+select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations 
+, sum(cast(vac.new_vaccinations as bigint)) over (partition by dea.location order by dea.location, dea.date) as RollingPeopleVaccinated
+from [Portfolio-Project].dbo.[CovidDeaths] dea
+join [Portfolio-Project].dbo.[CovidVaccinations] vac
+on dea.location = vac.location
+and dea.date = vac.date
+where dea.continent is not null
+--order by 2,3
+
+Select *, (RollingPeopleVaccinated/Population)*100
+from #PercentPopulationVaccinated
+
+-- Creating view to store data for later visualization 
+
+Create view PercentPopulationVaccinated as
+select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations 
+, sum(cast(vac.new_vaccinations as bigint)) over (partition by dea.location order by dea.location, dea.date) as RollingPeopleVaccinated
+from [Portfolio-Project].dbo.[CovidDeaths] dea
+join [Portfolio-Project].dbo.[CovidVaccinations] vac
+on dea.location = vac.location
+and dea.date = vac.date
+where dea.continent is not null
